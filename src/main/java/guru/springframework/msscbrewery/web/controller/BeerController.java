b91661d9d2052132d51ef80b493ad19c6b3b8c4a
John Strain <jjstrain68@gmail.com>
2021-01-07 11:55:24 +0000

    Added BeerControllerTest and validations on BeerDto

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -2,13 +2,12 @@ package guru.springframework.msscbrewery.web.controller;
 
 import guru.springframework.msscbrewery.services.BeerService;
 import guru.springframework.msscbrewery.web.model.BeerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 /**
@@ -30,4 +29,31 @@ public class BeerController {
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
     }
 
+    @PostMapping // Create a new beer
+    public ResponseEntity handlePost(@Valid @RequestBody BeerDto beerDto){
+
+        BeerDto savedBeerDto = beerService.saveNewBeer(beerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        // ToDo add hostname to the URL. For now relative path without hostname
+        headers.add( "Location", "/api/v1/beer" + savedBeerDto.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/{beerId}"}) // Update a beer
+    public ResponseEntity handleUpdate(@Valid @PathVariable("beerId") UUID beerId,@RequestBody BeerDto beerDto){
+
+        beerService.updateBeer(beerId, beerDto);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+
+    }
+
+    @DeleteMapping({"/{beerId}"}) // Delete a beer
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeer(@PathVariable("beerId") UUID beerId){
+
+        beerService.deleteById(beerId);
+    }
 }
--- src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -3,12 +3,10 @@ package guru.springframework.msscbrewery.web.controller;
 import guru.springframework.msscbrewery.services.CustomerService;
 import guru.springframework.msscbrewery.web.model.BeerDto;
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -24,5 +22,30 @@ public class CustomerController {
     public ResponseEntity<CustomerDto> getCustomer(@PathVariable("customerId") UUID customerId){
 
         return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
+    }
+
+    @PostMapping //Create a new customer
+    public ResponseEntity handlePost(@RequestBody CustomerDto customerDto){
+        CustomerDto savedCustomerDto = customerService.saveNewCustomer(customerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        // ToDo add hostname to the URL. For now relative path without hostname
+        headers.add( "Location", "/api/v1/customer" + savedCustomerDto.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/{customerId}"}) //Update a customer
+    public ResponseEntity handleUpdate(@PathVariable("customerId") UUID customerId,@RequestBody CustomerDto customerDto){
+
+        customerService.updateCustomer(customerId,customerDto);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping({"/{customerId}"}) //Delete a customer
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomer(@PathVariable("customerId") UUID customerId){
+        customerService.deleteById(customerId);
     }
 }
